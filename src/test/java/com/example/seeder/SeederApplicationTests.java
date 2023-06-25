008414ef1ad301810aa7e00b355917611b878c14
manoj kumar <manoj.k@zemosolabs.com>
2023-06-25 11:52:54 +0530

    seeder service

---
+++ src/test/java/com/example/seeder/SeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeederApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
